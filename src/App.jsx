748aca929db009f40a4eb03dc0eec7a7f3dd69ec
Imad Bengreine <imadbengriene@gmail.com>
2026-03-04 16:14:59 +0000

    Initial commit: React + Vite setup

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import {useState} from 'react'
+
+function App() {
+  const [todos, ]
+  return (
+    <>
+      <h1>To Do List</h1>
+      <input type="text" name="task" />
+      <button>Add task</button>
+
+      <div className="">
+
+      </div>
+    </>
+  );
+}
+
+export default App;
